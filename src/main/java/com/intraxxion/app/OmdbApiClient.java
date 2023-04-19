f4c7792e9a7aa69f5326c709b728a69e12d98c12
Thijs Creemers <thijs.creemers@gmail.com>
2023-04-19 11:12:30 +0200

    Initial

--- src/main/java/com/intraxxion/app/OmdbApiClient.java
+++ src/main/java/com/intraxxion/app/OmdbApiClient.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpClient.Redirect;
-import java.net.Authenticator;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
